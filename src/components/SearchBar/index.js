f6cd1c8c26bd8a80f46014f64ae483a31338629a
hung <hungtrewq3214@gmail.com>
2022-06-08 20:11:54 +0700

    Button clear search

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,6 +1,6 @@
 import classNames from 'classnames/bind';
-import { useState, useEffect } from 'react';
-import { SearchOutlined } from '@ant-design/icons';
+import { useState, useEffect, useRef } from 'react';
+import { CloseCircleOutlined, SearchOutlined } from '@ant-design/icons';
 
 import styles from './SearchBar.module.scss';
 
@@ -8,6 +8,8 @@ const cx = classNames.bind(styles);
 
 export default function SearchBar({ resSearch, callBackChildren }) {
     const [searchValue, setSearchValue] = useState('');
+
+    const inputRef = useRef();
 
     useEffect(() => {
         const results = resSearch.filter(
@@ -19,13 +21,28 @@ export default function SearchBar({ resSearch, callBackChildren }) {
         callBackChildren(results, !!searchValue);
     }, [searchValue]);
 
+    const handleClear = () => {
+        setSearchValue('');
+
+        inputRef.current.focus();
+    };
+
     return (
         <div className={cx('wrapper')}>
             <input
+                ref={inputRef}
                 value={searchValue}
                 className={cx('search-input')}
-                placeholder='Search your Pokemon'
-                onChange={(e) => setSearchValue(e.target.value)}></input>
+                placeholder="Search your Pokemon"
+                onChange={(e) => setSearchValue(e.target.value)}
+            ></input>
+
+            {!!searchValue && (
+                <span className={cx('search-clear')}>
+                    <CloseCircleOutlined onClick={handleClear} />
+                </span>
+            )}
+
             <button className={cx('search-btn')}>
                 <SearchOutlined />
             </button>
